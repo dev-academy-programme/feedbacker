170f34cb30cdc070a25d9e3bfe25f65b07dc7c8a
Rich Churcher <rich.churcher@gmail.com>
2016-11-28 11:35:20 +1300

    Remove Question

--- src/server/Question.js
+++
@@ -1,7 +0,0 @@
-export default function Question (raw) {
-  if (raw.hasOwnProperty('userEnteredValue')) {
-    const [_, designator, title] = /(\d.\d) (.*)/.exec(raw.userEnteredValue.stringValue)
-    return { designator, title }
-  }
-  return null
-}
