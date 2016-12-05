785ee71c656cdba013d898c98bf7f98bf1c9678d
Rich Churcher <rich.churcher@gmail.com>
2016-12-05 15:10:31 +1300

    Remove Hello, add Cohort Chooser

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <cohort-chooser></cohort-chooser>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import CohortChooser from './components/CohortChooser'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    CohortChooser
   }
 }
 </script>
---
+++ src/components/CohortChooser.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cohort-chooser">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cohort-chooser',
+  data () {
+    return {
+      msg: 'COHORT-CHOOSER'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: cornflowerblue;
+}
+</style>
